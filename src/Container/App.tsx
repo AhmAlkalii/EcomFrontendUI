4e69ef5bdc69991f1baf2052a3dcfe5acc299652
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-10-25 18:17:15 +0200

    ShoppingCart Done✅

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header, Footer } from '../Components/Layout';
-import { Home, MenuItemDetails, NotFound } from '../Pages';
+import { Home, MenuItemDetails, NotFound, ShoppingCart } from '../Pages';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useGetShoppingCartQuery } from '../Apis/shoppingCartApi';
+import { setShoppingCart } from '../Storage/Redux/shoppingCartSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const {data, isLoading} = useGetShoppingCartQuery("4207f181-b34f-4900-b193-453711619ff9")
 
+  useEffect(() => {
+    if(!isLoading){
+      console.log(data.result)
+      dispatch(setShoppingCart(data.result?.cartItems))
+    }
+  },[data])
+
+   
   return (
     <div>    
       <Header/>
@@ -12,6 +25,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/menuItemDetails/:menuItemId' element={<MenuItemDetails/>}/>
+          <Route path='/shoppingCart' element={<ShoppingCart />}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
       </div>
--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,3 +1,6 @@
 import menuItemModel from "./menuItemModel";
+import cartItemModel from "./cartItemModel";
+import shoppingCarttModel from "./shoppingCarttModel";
 
-export type {menuItemModel}
+
+export type {menuItemModel, cartItemModel, shoppingCarttModel}
